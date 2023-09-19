83a34c139b220fd6f8cb0fc01f2f8583c2f0d6e6
AzharBhyat <57112010+AzharBhyat@users.noreply.github.com>
2023-09-19 13:37:53 +0200

    Update mpthree.py

--- src/mpthree.py
+++ src/mpthree.py
@@ -11,9 +11,27 @@ def mpsearch(_query, _reslimit):
     return(videosSearch.result()["result"])
 
 def mpdownload(url):
-    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio'}) as video:
-        info_dict = video.extract_info(url, download = False)
-        song_title = info_dict['title']
-        song_url = info_dict['url'] 
-    print("Successfully Downloaded ")
-    return(song_title, song_url)
+    ydl_opts = {
+        'format': 'bestaudio/best[ext=mp3]/bestaudio/best[abr=128]',  # Get the best quality audio format
+        'force_generic_extractor': True,  # Use the generic extractor to avoid downloading
+        'simulate': True,  # Simulate the download
+        'quiet': False,  # Suppress output
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(url, download=False)
+            audio_url = info_dict['url']
+            
+            with ydl.urlopen(audio_url) as response:
+                audio_data = b''  # Initialize an empty bytes object
+                while True:
+                    chunk = response.read(8192)  # Read in chunks of 8192 bytes
+                    if not chunk:
+                        break
+                    audio_data += chunk
+                    
+                return audio_data, info_dict['title']
+        except yt_dlp.DownloadError as e:
+            print("Error:", e)
+            return None
